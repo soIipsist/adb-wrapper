ae6f8b9ae8217d24e83ad0013358067fc90f34ae
soIipsist <solipsist14@gmail.com>
2024-11-20 16:47:40 +0200

    utils - download_sdk_platform_tools

--- adb/adb.py
+++ adb/adb.py
@@ -1,10 +1,6 @@
 import subprocess
 import shlex
-import time
 import os
-import json
-import wget
-import sys
 from typing import List
 from .utils import *
 from functools import wraps
@@ -12,6 +8,29 @@ import shutil
 from importlib import resources
 import io
 import platform
+
+
+def command(command, *args, **kwargs):
+    def _command(f):
+        def wrapper(self, *args, **kwargs):
+
+            return
+
+        return wrapper
+
+    return _command
+
+
+class Package:
+    def __init__(
+        self,
+        img_src: str = None,
+        package_name: str = None,
+        name: str = None,
+        genre: str = None,
+        privileged: bool = False,
+    ):
+        pass
 
 
 class ADB(object):
@@ -22,45 +41,25 @@ class ADB(object):
 
     def check_adb_path(self):
         """
-        Returns base adb command if PATH variable was found.    
+        Returns base adb command if PATH variable was found.
         """
         # check if PATH variable was found
-        path = os.environ.get('path')
+        path = os.environ.get("path")
 
-        if 'platform-tools' not in path.lower():
+        if "platform-tools" not in path.lower():
             user_input = input(
-                'adb was not found as a PATH variable. Would you like to download the latest version of sdk platform-tools? (y/n)')
-            if user_input == 'y':
-                download_dir = input('Download directory: ')
+                "adb was not found as a PATH variable. Would you like to download the latest version of sdk platform-tools? (y/n)"
+            )
+            if user_input == "y":
+                download_dir = input("Download directory: ")
                 sdk_path = download_sdk_platform_tools(download_dir)
                 return sdk_path
             else:
                 raise FileNotFoundError(
-                    'Cannot execute adb commands, please add platform-tools to your PATH variable.')
+                    "Cannot execute adb commands, please add platform-tools to your PATH variable."
+                )
 
-        return 'adb'
-
-    def get_packages_from_json(self, file_name: str, key=None):
-        """
-        Given a .json file, return the data.
-        If a key is specified, it will return value for key if key is in the dictionary, else 
-        all the data.
-
-        """
-        if isinstance(file_name, io.TextIOWrapper):
-            file_name = file_name.name
-
-        if not file_name.endswith('.json'):
-            raise FileNotFoundError(
-                'Invalid file type provided. File must be in .json format.')
-
-        packages = read_json_file(file_name)
-        if key:
-            l = []
-            for p in packages:
-                l.append(p.get(key))
-            return l
-        return packages
+        return "adb"
 
     def get_devices(self):
         """
@@ -90,21 +89,21 @@ class ADB(object):
             if isinstance(command_args, str):
                 command_args = shlex.split(command_args)
 
-            if not isinstance(command_args, list) or any(not isinstance(arg, str) for arg in command_args):
-                raise TypeError(
-                    "The command passed is not a list of strings."
-                )
+            if not isinstance(command_args, list) or any(
+                not isinstance(arg, str) for arg in command_args
+            ):
+                raise TypeError("The command passed is not a list of strings.")
 
             command_args.insert(0, self.command)
             if device_id:
-                command_args.insert(1, '-s')
+                command_args.insert(1, "-s")
                 command_args.insert(2, device_id)
 
             process = subprocess.Popen(
-                command_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                command_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+            )
             output = (
-                process
-                .communicate(timeout=None)[0]
+                process.communicate(timeout=None)[0]
                 .strip()
                 .decode(errors="backslashreplace")
             )
@@ -121,8 +120,7 @@ class ADB(object):
 
 class Device(ADB):
     id = None
-    settings = {"system_settings": [],
-                "global_settings": [], "secure_settings": []}
+    settings = {"system_settings": [], "global_settings": [], "secure_settings": []}
     system_packages = []
     third_party_packages = []
     do_not_delete_packages = []
@@ -139,21 +137,22 @@ class Device(ADB):
 
     def download_sdk_platform_tools(self):
         raise AttributeError(
-            "'Device' object has no attribute 'download_sdk_platform_tools'")
+            "'Device' object has no attribute 'download_sdk_platform_tools'"
+        )
 
     def get_model(self):
-        return self.get_shell_property('ro.product.model')
+        return self.get_shell_property("ro.product.model")
 
     def get_name(self):
-        return self.get_shell_property('ro.product.name')
+        return self.get_shell_property("ro.product.name")
 
     def get_sdk(self):
-        return self.get_shell_property('ro.build.version.sdk')
+        return self.get_shell_property("ro.build.version.sdk")
 
     def get_packages(self):
         packages = []
-        self.system_packages = self.get_system_packages().split('\n')
-        self.third_party_packages = self.get_third_party_packages().split('\n')
+        self.system_packages = self.get_system_packages().split("\n")
+        self.third_party_packages = self.get_third_party_packages().split("\n")
         packages.extend(self.system_packages)
         packages.extend(self.third_party_packages)
         return packages
@@ -165,7 +164,9 @@ class Device(ADB):
                     params = str(*arguments).format(*args)
                     return self.execute(params)
                 return self.execute(*arguments)
+
             return wrapper
+
         return _command
 
     def get_shell_property(self, prop):
@@ -175,8 +176,11 @@ class Device(ADB):
         system_settings = self.get_system_settings()
         global_settings = self.get_global_settings()
         secure_settings = self.get_secure_settings()
-        self.settings.update(system_settings=system_settings,
-                             global_settings=global_settings, secure_settings=secure_settings)
+        self.settings.update(
+            system_settings=system_settings,
+            global_settings=global_settings,
+            secure_settings=secure_settings,
+        )
         return self.settings
 
     @command("shell settings list system")
@@ -235,7 +239,7 @@ class Device(ADB):
     def expand_notifications(self):
         pass
 
-    @command('shell locksettings set-disabled true')
+    @command("shell locksettings set-disabled true")
     def disable_lock_screen(self):
         pass
 
@@ -288,7 +292,7 @@ class Device(ADB):
         for p in permissions:
             self.grant_permission(package, p)
 
-    def push_files(self, pc_files: List[str], device_path='/sdcard'):
+    def push_files(self, pc_files: List[str], device_path="/sdcard"):
         for f in pc_files:
             self.push_file(f, device_path)
 
@@ -302,34 +306,34 @@ class Device(ADB):
 
     def google_debloat(self):
         file = None
-        with resources.open_text('lists', 'google.json') as file:
-            google_packages = self.get_packages_from_json(file, 'package_name')
+        with resources.open_text("lists", "google.json") as file:
+            google_packages = self.get_packages_from_json(file, "package_name")
             self.uninstall_packages(google_packages)
 
     def backup(self, shared_storage=False, apks=False, system=False, path: str = None):
-        cmd = ['backup', '-all']
+        cmd = ["backup", "-all"]
 
         if shared_storage:
-            cmd.append('-shared')
+            cmd.append("-shared")
 
         if apks:
-            cmd.append('-apk')
+            cmd.append("-apk")
 
         if system:
-            cmd.append('-system')
+            cmd.append("-system")
 
         if path:
-            cmd.append('-f')
+            cmd.append("-f")
             # check if valid directory
             directory = os.path.dirname(path)
             file_name = os.path.basename(path)
             print(directory, file_name)
 
             if not os.path.isdir(directory):
-                raise FileNotFoundError('Directory not valid')
+                raise FileNotFoundError("Directory not valid")
 
-            if not file_name.endswith('.ab'):
-                raise FileNotFoundError('Backup files must be of .ab type.')
+            if not file_name.endswith(".ab"):
+                raise FileNotFoundError("Backup files must be of .ab type.")
             cmd.append(path)
 
         self.execute(cmd)
@@ -339,49 +343,17 @@ class Device(ADB):
         # a) namespace.key=value
         # b) namespace.key value
         # c) namespace.key.other_value value
-        input = input.replace('=', '.', 1).replace(' ', '.').split('.')
+        input = input.replace("=", ".", 1).replace(" ", ".").split(".")
         namespace = input[0]
         key = input[1:-1]
-        key = key[0] if len(key) == 1 else str.join('.', key)
+        key = key[0] if len(key) == 1 else str.join(".", key)
         value = input[-1]
         output = "{0} {1} {2}".format(namespace, key, value)
 
         return output
 
-    def set_settings(self,  settings: List[str]):
+    def set_settings(self, settings: List[str]):
         for setting in settings:
             setting_cmd = self.get_setting_cmd(setting)
             cmd = "shell settings put {0}".format(setting_cmd)
             self.execute(cmd)
-
-
-def download_sdk_platform_tools(output_directory=None):
-    """
-    Can be used to easily download SDK platform-tools, if adb doesn't exist as a
-    PATH variable.
-
-    """
-
-    if not output_directory:
-        print('No directory found, using default home directory.')
-        output_directory = os.path.expanduser('~')
-
-    download_link = 'https://dl.google.com/android/repository/platform-tools-latest-{0}.zip'.format(
-        platform.system().lower())
-
-    # download sdk platform tools
-    try:
-        print('Downloading SDK platform tools...')
-        sdk_path = wget.download(download_link, out=output_directory)
-
-        # unzip
-        shutil.unpack_archive(sdk_path, output_directory)
-        sdk_path = sdk_path.replace('.zip', '').replace('/', '\\')
-
-        print()
-        print(
-            "SDK platform-tools was successfully downloaded at '{0}'.".format(output_directory))
-    except Exception as e:
-        print(e)
-
-    return sdk_path
--- adb/utils.py
+++ adb/utils.py
@@ -1,4 +1,9 @@
+import io
 import json
+import os
+import shutil
+import platform
+
 
 def create_json_file(json_file, data=None):
     try:
@@ -8,11 +13,50 @@ def create_json_file(json_file, data=None):
             json.dump(data, file)
     except Exception as e:
         print(e)
-        
+
+
 def read_json_file(json_file, errors=None):
     try:
-        with open(json_file, 'r', errors=errors) as file:
+        with open(json_file, "r", errors=errors) as file:
             json_object = json.load(file)
             return json_object
     except Exception as e:
         print(e)
+
+
+def download_sdk_platform_tools(output_directory=None):
+    """
+    Can be used to easily download SDK platform-tools, if adb doesn't exist as a
+    PATH variable.
+
+    """
+
+    if not output_directory:
+        print("No directory found, using default home directory.")
+        output_directory = os.path.expanduser("~")
+
+    download_link = (
+        "https://dl.google.com/android/repository/platform-tools-latest-{0}.zip".format(
+            platform.system().lower()
+        )
+    )
+
+    # download sdk platform tools
+    try:
+        print("Downloading SDK platform tools...")
+        sdk_path = wget.download(download_link, out=output_directory)
+
+        # unzip
+        shutil.unpack_archive(sdk_path, output_directory)
+        sdk_path = sdk_path.replace(".zip", "").replace("/", "\\")
+
+        print()
+        print(
+            "SDK platform-tools was successfully downloaded at '{0}'.".format(
+                output_directory
+            )
+        )
+    except Exception as e:
+        print(e)
+
+    return sdk_path
