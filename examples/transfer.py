eddb725363bd939c470dc0bc10421626e2f9ecb0
soIipsist <solipsist14@gmail.com>
2025-05-03 13:18:13 +0300

    transfer.py

--- examples/transfer.py
+++ examples/transfer.py
@@ -41,35 +41,46 @@ if __name__ == "__main__":
 
     idx = 0
     while idx < len(source_files):
-
         try:
             source_file = source_files[idx]
-            source_file = quote(source_file)
 
+            # Local PC directory
             if os.path.isdir(source_file):
-                new_source_files = [
-                    entry.path for entry in os.scandir(source_file) if entry.is_file()
-                ]
+                new_source_files = []
+                for root, dirs, files in os.walk(source_file):
+                    for file in files:
+                        new_source_files.append(os.path.join(root, file))
                 source_files[idx : idx + 1] = new_source_files
 
+            # Remote device directory
             elif device.is_directory(source_file):
-                new_source_files = [
-                    os.path.join(source_file, file)
-                    for file in device.get_files_in_directory(source_file)
-                ]
+                new_source_files = []
+
+                def walk_device_dir(path):
+                    entries = device.get_files_in_directory(path)
+                    for entry in entries:
+                        full_path = os.path.join(path, entry)
+                        if device.is_directory(full_path):
+                            walk_device_dir(full_path)
+                        else:
+                            new_source_files.append(full_path)
+
+                walk_device_dir(source_file)
                 source_files[idx : idx + 1] = new_source_files
 
+            # File (push/pull)
             else:
                 dest_file = os.path.join(
                     destination_directory, os.path.basename(source_file)
                 )
 
-                if is_push:  # transfer from pc to device
+                if is_push:  # transfer from PC to device
                     output = device.push_file(source_file, dest_file)
-                else:  # transfer from device to pc
+                else:  # transfer from device to PC
                     output = device.pull_file(source_file, dest_file)
 
                 idx += 1
+
         except Exception as e:
             print("Exception:", e)
             idx += 1
