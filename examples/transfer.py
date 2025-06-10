754cc531f3b19b73add2b5ce2a03fdb454702453
soIipsist <solipsist14@gmail.com>
2025-06-10 12:06:22 +0300

    transfer script simplifications

--- examples/transfer.py
+++ examples/transfer.py
@@ -1,86 +1,58 @@
-from adb_wrapper.adb import ADB  # change this to from adb_wrapper.adb
+import sys
+from pathlib import Path
+
+project_root = Path(__file__).resolve().parent.parent
+
+if str(project_root) not in sys.path:
+    sys.path.insert(0, str(project_root))
+
+from adb_wrapper.adb import ADB
 import argparse
 import os
-from shlex import quote
 
 """A script designed to transfer files from your device to your pc and vice versa."""
+
+
+def get_pc_directory_files(pc_directory: str):
+    source_files = [
+        os.path.join(pc_directory, f)
+        for f in os.listdir(pc_directory)
+        if os.path.isfile(os.path.join(pc_directory, f))
+    ]
+
+    return source_files
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("source_files", nargs="+", type=str)
-    parser.add_argument("--destination_directory", type=str, default="/sdcard/Download")
-    parser.add_argument("-i", "--device_ip", type=str, default=None)
+    parser.add_argument("source_directory", type=str)
+    parser.add_argument(
+        "destination_directory", type=str, default="/sdcard/Download", nargs="?"
+    )
+    parser.add_argument("-n", "--nested_files", default=False)
 
     args = vars(parser.parse_args())
-    source_files = args.get("source_files")
+    source_directory = args.get("source_directory")
     destination_directory = args.get("destination_directory")
-    device_ip = args.get("device_ip")
+    nested_files = args.get("nested_files")
 
-    adb = ADB()
+    print(args)
+    # adb = ADB()
+    # device = adb.get_device()
 
-    if device_ip:
-        adb.connect(device_ip)
+    # if not device:
+    #     raise ValueError("Device not found. Please connect your device via adb.")
 
-    device = adb.get_device()
+    is_push = os.path.exists(source_directory)  # indicates these are pc files
+    source_files = []
 
-    if not device:
-        raise ValueError("Device not found. Please connect your device via adb.")
+    if is_push:
+        source_files = get_pc_directory_files(source_directory)
+        # device.push_files(source_files, destination_directory=destination_directory)
+    else:
+        pass
+        # source_files = device.get_files_in_directory(source_directory)
+        # device.pull_files(source_files, destination_directory=destination_directory)
 
-    is_push = os.path.exists(source_files[0])  # indicates these are pc files
-
-    # check if destination directory is valid
-    if destination_directory is None:
-        raise ValueError("Destination directory must be given.")
-
-    if destination_directory:
-        if is_push:
-            assert device.is_directory(destination_directory)
-        else:
-            assert os.path.isdir(destination_directory)
-
-    idx = 0
-    while idx < len(source_files):
-        try:
-            source_file = source_files[idx]
-
-            # Local PC directory
-            if os.path.isdir(source_file):
-                new_source_files = []
-                for root, dirs, files in os.walk(source_file):
-                    for file in files:
-                        new_source_files.append(os.path.join(root, file))
-                source_files[idx : idx + 1] = new_source_files
-
-            # Remote device directory
-            elif device.is_directory(source_file):
-                new_source_files = []
-
-                def walk_device_dir(path):
-                    entries = device.get_files_in_directory(path)
-                    for entry in entries:
-                        full_path = os.path.join(path, entry)
-                        if device.is_directory(full_path):
-                            walk_device_dir(full_path)
-                        else:
-                            new_source_files.append(full_path)
-
-                walk_device_dir(source_file)
-                source_files[idx : idx + 1] = new_source_files
-
-            # File (push/pull)
-            else:
-                dest_file = os.path.join(
-                    destination_directory, os.path.basename(source_file)
-                )
-
-                if is_push:  # transfer from PC to device
-                    output = device.push_file(source_file, dest_file)
-                else:  # transfer from device to PC
-                    output = device.pull_file(source_file, dest_file)
-
-                idx += 1
-
-        except Exception as e:
-            print("Exception:", e)
-            idx += 1
+    print("Source files: ", source_files)
